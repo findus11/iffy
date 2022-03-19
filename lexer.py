f6cd61192a9d6fea138bf034a69b612feb8df173
findus11 <findusxi@protonmail.com>
2022-03-19 12:10:45 +0100

    did you think i was done with the lexer?

--- lexer.py
+++ lexer.py
@@ -3,28 +3,35 @@ from importlib import import_module
 from span import Span
 from typing import Any, Callable, Optional
 
+
 class Tt:
     pass
+
 
 def is_ws(c: str) -> bool:
     return c.isspace() and c != '\n'
 
+
 def is_id(c: str) -> bool:
     return c.isalpha() or c.isnumeric() or c == '\'' or c == '_'
 
+
 def is_id_start(c: str) -> bool:
-    return c.isalpha() or c == '_' or c =='\''
+    return c.isalpha() or c == '_' or c == '\''
+
 
 def is_num(c: str) -> bool:
     return c.isdecimal() or c == '_' or c == '\''
+
 
 class UnknownTokenError(Exception):
     def __init__(self, type: str, lexeme: str):
         self.type = type
         self.lexeme = lexeme
-    
+
     def __str__(self) -> str:
         return f"unknown {self.type}: {repr(self.lexeme)}"
+
 
 class Token:
     def __init__(self, type: Tt, span: Span, lit: str = "", val: Any = None):
@@ -32,10 +39,10 @@ class Token:
         self.lit = lit
         self.span = span
         self.val = val
-    
+
     def __str__(self) -> str:
         return str(self.lit)
-    
+
     def __repr__(self) -> str:
         r = f"<Token {self.type}"
         if len(self.lit) > 0:
@@ -44,6 +51,7 @@ class Token:
             r += " " + repr(self.val)
         return r + ">"
 
+
 class Lexer:
     """
     The lexer is responsible for turning a string of characters into a series of
@@ -51,9 +59,8 @@ class Lexer:
     """
 
     class State(Enum):
-        START    = 0
-        FOR_QUAL = 1
-        ESCAPE   = 2
+        START = 0
+        ESCAPE = 1
 
     def __init__(self, src: str):
         self.src = src
@@ -65,7 +72,7 @@ class Lexer:
         self.idents: Callable[[str, Lexer], Tt] = []
         self.numbers: Callable[[str, Lexer], Tt] = []
         self.strings: Callable[[str, Lexer], Tt] = []
-        self.ops: Callable[[str, Lexer], Tt] = []
+        self.ops: Callable[[Lexer], Tt] = []
 
     def refeed(self, new_src: str):
         self.src = new_src
@@ -78,21 +85,21 @@ class Lexer:
         self.numbers.append(tt.number)
         self.strings.append(tt.string)
         self.ops.append(tt.op)
-    
+
     def id_type(self, lexeme: str) -> Tt:
         for ident in reversed(self.idents):
             tt = ident(lexeme, self)
             if tt is not None:
                 return tt
         raise UnknownTokenError("identifier", lexeme)
-    
+
     def num_type(self, lexeme: str) -> Tt:
         for number in reversed(self.numbers):
             tt = number(lexeme, self)
             if tt is not None:
                 return tt
         raise UnknownTokenError("number", lexeme)
-    
+
     def str_type(self, lexeme: str) -> Tt:
         for string in reversed(self.strings):
             tt = string(lexeme, self)
@@ -100,32 +107,32 @@ class Lexer:
                 return tt
         raise UnknownTokenError("string", lexeme)
 
-    def op_type(self, lexeme) -> Tt:
+    def op_type(self) -> Tt:
         for op in reversed(self.ops):
-            tt = op(lexeme, self)
+            tt = op(self)
             if tt is not None:
                 return tt
-        raise UnknownTokenError("operator", lexeme)
-    
+        raise UnknownTokenError("operator", self.curr())
+
     def curr(self) -> Optional[str]:
         if self.done:
             return None
         return self.src[self.currn]
-    
+
     def next(self) -> Optional[str]:
         if self.currn >= len(self.src) - 1 or self.done:
             self.currn += 1
             self.done = True
             return None
-        
+
         self.currn += 1
         return self.src[self.currn]
-    
+
     def peek(self) -> Optional[str]:
         if self.currn >= len(self.src) - 1 or self.done:
             return None
         return self.src[self.currn + 1]
-    
+
     def sub(self) -> str:
         return self.src[self.start:self.currn]
 
@@ -134,12 +141,19 @@ class Lexer:
         span = Span(self.start, self.currn)
         self.start = self.currn
         return Token(type, span, lit, val)
-    
+
+    def move_on(self, expect: str | list[str]) -> bool:
+        c = self.peek()
+        if c in expect:
+            self.next()
+            return True
+        return False
+
     def move_while(self, f: Callable[[str], bool]):
         c = self.curr()
         while not self.done and f(c):
             c = self.next()
-    
+
     def skip_ws(self):
         self.move_while(is_ws)
         self.consume(None)
@@ -147,7 +161,7 @@ class Lexer:
     def ident(self) -> Token:
         self.move_while(is_id)
         return self.consume(self.id_type(self.sub()))
-    
+
     def number(self) -> Token:
         self.move_while(is_num)
         lit = self.sub()
@@ -175,21 +189,26 @@ class Lexer:
 
             val += c
             c = self.next()
-        
+
         # Consume the final "
         self.next()
         return self.consume(self.str_type(self.sub()), val)
-    
+
+    # def op(self) -> Token:
+    #     self.move_while(lambda c:
+    #                     not (is_ws(c) or is_id(c) or is_num(c) or c == '"')
+    #                     )
+    #     return self.consume(self.op_type(self.sub()))
+
     def op(self) -> Token:
-        self.move_while(lambda c: 
-            not (is_ws(c) or is_id(c) or is_num(c) or c == '"')
-        )
-        return self.consume(self.op_type(self.sub()))
-    
+        tt = self.op_type()
+        self.next()  # Consume the op
+        return self.consume(tt)
+
     def line_comment(self):
         self.move_while(lambda c: c != '\n')
         self.consume(None)
-    
+
     def lex(self) -> list[Token]:
         res = []
         while not self.done:
@@ -214,13 +233,14 @@ class Lexer:
             res.append(self.op())
         return res
 
+
 if __name__ == "__main__":
     print("Ctrl+D to exit, `--plug <module>` to add plugin")
     l = Lexer("")
 
     while True:
         query: str
-        
+
         try:
             query = input("> ")
         except EOFError:
@@ -248,4 +268,3 @@ if __name__ == "__main__":
             print(e)
             continue
         print(res)
-
--- std/token.py
+++ std/token.py
@@ -2,23 +2,132 @@ from enum import Enum
 from typing import Optional
 import lexer as l
 
+
+class State(Enum):
+    FOR_QUAL = 0
+
+
 class Tt(l.Tt, Enum):
-    NAME    = 0
-    NUMBER  = 1
-    STRING  = 2
-    OP      = 3
-    NEWLINE = 4
+    NAME = 0
+    NUMBER = 1
+    STRING = 2
+    NEWLINE = 3
+
+    IS = 10
+    LET = 11
+    VAL = 12
+    VAR = 13
+    SET = 14
+    TYPE = 15
+
+    RETURN = 30
+    BREAK = 31
+    CONTINUE = 32
+    THEN = 33
+
+    DO = 50
+    END = 51
+    IF = 52
+    ELSE = 53
+    FOR = 54
+    ALL = 55
+    ANY = 56
+    IN = 57
+    WHILE = 58
+
+    AND = 80
+    OR = 81
+    XOR = 82
+    NOT = 83
+    MOD = 84
+
+    LPAREN = 100
+    RPAREN = 101
+    LBRACKET = 102
+    RBRACKET = 103
+    LBRACE = 104
+    RBRACE = 105
+
+    PLUS = 110
+    MINUS = 111
+    STAR = 112
+    SLASH = 113
+    STAR_STAR = 114
+
+    EQUALS = 120
+    SLASH_EQUALS = 121
+    GREATER = 122
+    GREATER_EQUALS = 123
+    LESS = 124
+    LESS_EQUALS = 125
 
     def ident(lexeme: str, lexer: l.Lexer) -> Optional[l.Tt]:
-        return Tt.NAME
-    
+        match lexeme, lexer.state:
+            case "is", _: return Tt.IS
+            case "let", _: return Tt.LET
+            case "val", _: return Tt.VAL
+            case "var", _: return Tt.VAR
+            case "set", _: return Tt.SET
+            case "type", _: return Tt.TYPE
+            case "return", _: return Tt.RETURN
+            case "break", _: return Tt.BREAK
+            case "continue", _: return Tt.CONTINUE
+            case "then", _: return Tt.THEN
+            case "do", _: return Tt.DO
+            case "end", _: return Tt.END
+            case "if", _: return Tt.IF
+            case "else", _: return Tt.ELSE
+            case "for", _:
+                lexer.state = State.FOR_QUAL
+                return Tt.FOR
+            case "all", State.FOR_QUAL:
+                lexer.state = l.Lexer.State.START
+                return Tt.ALL
+            case "any", State.FOR_QUAL:
+                lexer.state = l.Lexer.State.START
+                return Tt.ANY
+            case "in", _: return Tt.IN
+            case "while", _: return Tt.WHILE
+            case "and", _: return Tt.AND
+            case "or", _: return Tt.OR
+            case "xor", _: return Tt.XOR
+            case "not", _: return Tt.NOT
+            case "mod", _: return Tt.MOD
+            case _: return Tt.NAME
+
     def number(lexeme: str, lexer: l.Lexer) -> Optional[l.Tt]:
         return Tt.NUMBER
-    
+
     def string(lexeme: str, lexer: l.Lexer) -> Optional[l.Tt]:
         return Tt.STRING
-    
-    def op(lexeme: str, lexer: l.Lexer) -> Optional[l.Tt]:
-        if lexeme == '\n':
-            return Tt.NEWLINE
-        return Tt.OP
+
+    def op(lexer: l.Lexer) -> Optional[l.Tt]:
+        c = lexer.curr()
+        match c:
+            case '\n': return Tt.NEWLINE
+            case '(': return Tt.LPAREN
+            case ')': return Tt.RPAREN
+            case '[': return Tt.LBRACKET
+            case ']': return Tt.RBRACKET
+            case '{': return Tt.LBRACE
+            case '}': return Tt.RBRACE
+            case '+': return Tt.PLUS
+            case '-': return Tt.MINUS
+            case '*':
+                if lexer.move_on('*'):
+                    return Tt.STAR_STAR
+                return Tt.STAR
+            case '/':
+                if lexer.move_on('='):
+                    return Tt.SLASH_EQUALS
+                return Tt.SLASH
+            case '=': return Tt.EQUALS
+            case '>':
+                if lexer.move_on('='):
+                    return Tt.GREATER_EQUALS
+                return Tt.GREATER
+            case '<':
+                if lexer.move_on('='):
+                    return Tt.LESS_EQUALS
+                return Tt.LESS
+            case _: return None
